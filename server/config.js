a7e12497d5845c7231b6416bc563cd9f2397b431
Khoa1107 <161037921+Khoa1107@users.noreply.github.com>
2024-02-23 20:20:08 +0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     // Game server domain.
     // If 'localhost:NUMBER', the port must equal the port setting.
-    host: "ProjectPhysical.glitch.me",
+    host: "polished-antique-rhodium.glitch.me",
 
     // Which port to run the web server on.
     port: 26301,
